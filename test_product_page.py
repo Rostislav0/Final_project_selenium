e16cdf49aa6531275322087702dee560a9521983
Rostislav0 <109548781+Rostislav0@users.noreply.github.com>
2022-10-23 16:38:11 +0300

    uncomment and amendments utf-8

--- test_product_page.py
+++ test_product_page.py
@@ -26,6 +26,7 @@ class TestUserAddToBasketFromProductPage():
         product_page = ProductPage(browser, page)
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = BasePage(browser, link)
@@ -41,11 +42,22 @@ class TestGuestCanGoToLoginPageFromProductPage():
         page.open()
         page.should_be_login_link()
 
+    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = BasePage(browser, link)
         page.open()
         page.go_to_login_page()
+
+
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = BasePage(browser, link)
+    page.open()
+    product_page = ProductPage(browser, page)
+    product_page.add_to_basket_and_check_that_names_and_prices_equal()
+
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -78,6 +90,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message_disappeared()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasePage(browser, link)
@@ -85,3 +98,4 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     basket_page = BasketPage(browser=browser, page=page)
     basket_page.is_basket_empty()
+
