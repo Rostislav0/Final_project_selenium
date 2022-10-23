0da296cd56f199a51291c6ab3e93026585271e11
Rostislav0 <109548781+Rostislav0@users.noreply.github.com>
2022-10-23 16:01:12 +0300

    add setup funct fixture about authorize user

--- test_product_page.py
+++ test_product_page.py
@@ -1,19 +1,33 @@
 import pytest
 from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+import time
 
 
 class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        link_login = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        page = BasePage(browser, link_login)
+        page.open()
+        login_page = LoginPage(browser)
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        login_page.register_new_user(email=email, password=password)
+        page.should_be_authorized_user()
 
-    def test_user_cant_see_success_message(browser):
+    def test_user_cant_see_success_message(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = BasePage(browser, link)
         page.open()
         product_page = ProductPage(browser, page)
         product_page.should_not_be_success_message()
 
-    def test_user_can_add_product_to_basket(browser, link):
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = BasePage(browser, link)
         page.open()
         product_page = ProductPage(browser, page)
